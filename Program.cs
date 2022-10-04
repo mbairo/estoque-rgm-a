6636eda3974ee193ff5b55e617096ed0bd0938e3
mbairo <bairo.marcos@gmail.com>
2022-10-04 11:08:38 -0300

    add nova versao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-//	dotnet add package Microsoft.EntityFrameworkCore.InMemory --version 6.0 && dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 6.0
+//	dotnet add package Microsoft.EntityFrameworkCore.InMemory --version 6.0 
 //	dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 6.0
 //	dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0
 	
